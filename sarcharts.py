698b863ea645352656a204c30294856f3b5fba91
Pablo Fernández Rodríguez <pafernan@redhat.com>
2024-05-01 12:35:07 +0200

    fix relative/absolute sar files paths

--- sarcharts.py
+++ sarcharts.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     Conf.get_opts()
     sarfiles = Util.get_sarfiles(Conf)
     showheader = ""
-    for f in sarfiles[-Conf.last:]:
-        inputfile = Conf.inputdir + "/" + f
+    for inputfile in sarfiles[-Conf.last:]:
         for k, v in Conf.charts.items():
             csvfile = Conf.outputdir + "sar/" + k + ".csv"
             Util.exec_command(Conf, f"sadf -dt {inputfile} -- {v['arg']}"
