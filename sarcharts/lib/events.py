ff4623774b42ab018314481360f0870f2c3834ba
Pablo Fernández Rodríguez <pafernan@redhat.com>
2024-05-18 15:56:13 +0200

    events minor fixes

--- sarcharts/lib/events.py
+++ sarcharts/lib/events.py
@@ -10,16 +10,17 @@ class Events:
             with open(args.eventfile) as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=';')
                 for row in csv_reader:
-                    if row[1] not in charts.keys():
-                        util.debug(
-                            args, 'W',
-                            f"Host '{row[1]}' from '{args.eventfile}'"
-                            + " not in 'sar' data.")
-                        continue
-                    charts[row[1]]['xlabels'].append(row[0])
-                    if row[2] not in charts[row[1]]['events']:
-                        charts[row[1]]['events'][row[2]] = []
-                    charts[row[1]]['events'][row[2]].append(
-                           {'date': row[0], 'text': row[3]})
+                    if not row[0].startswith("#"):
+                        if row[1] not in charts.keys():
+                            util.debug(
+                                args, 'W',
+                                f"Host '{row[1]}' from '{args.eventfile}'"
+                                + " not in 'sar' data.")
+                            continue
+                        charts[row[1]]['xlabels'].append(row[0])
+                        if row[2] not in charts[row[1]]['events']:
+                            charts[row[1]]['events'][row[2]] = []
+                        charts[row[1]]['events'][row[2]].append(
+                               {'date': row[0], 'text': row[3]})
 
         return charts
